02b5609c2cdbb0ad0c8a570be936eb89d2f5308c
taqwa19-meet <taqwa19@meet.mit.edu>
2017-07-26 18:27:28 +0300

    Stopping here

--- caugh_speeding.py
+++ caugh_speeding.py
@@ -1,5 +1,8 @@
-speed=50
-is_birthday= True
+speed=40
+is_birthday= False
 if speed<31:
-    print(no ticket!)
-elif
+    print('no ticket!')
+elif speed>30 and speed<51:
+    print('small ticket')
+else:
+    print('big ticket')
